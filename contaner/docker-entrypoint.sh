5881acc11a654df3bcc026238ab16206b3db5867
Naba Kumar Das <nabad600@gmail.com>
2022-03-09 04:13:25 +0530

    Code update

--- contaner/docker-entrypoint.sh
+++ contaner/docker-entrypoint.sh
@@ -7,7 +7,12 @@ if [ ! -e index.php ] && [ ! -e wp-includes/version.php ]; then
                 echo >&2 "Complete! WordPress has been successfully copied to $PWD"
 fi
 
-cp /app/default.conf /etc/nginx/conf.d/default.conf
+if [[ {BACK_END} = nginx ]] ;
+then
+    cp /app/default.conf /etc/nginx/conf.d/default.conf
+else
+    cp /app/httpd.conf /etc/apache2/httpd.conf
+fi
 rm -rf /var/preview
 if [[ {USER_ID} -gt 0 ]] ;
 then
@@ -15,7 +20,15 @@ then
 else
     chown -R nobody:nobody /var/www
 fi
-pkill -9 php
-nginx -s reload
+
+if [[ {BACK_END} = nginx ]]  ; 
+then
+    nginx -s reload
+else
+    httpd -k graceful
+fi
+
+# pkill -9 php
+# nginx -s reload
 
 exec "$@"
