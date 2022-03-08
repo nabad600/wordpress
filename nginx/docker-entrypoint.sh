70cf26e5bf5de218adfc4f5bf113665d4b09e849
Naba Das <nabakr.das@Nabas-Mac-mini.local>
2022-03-08 20:28:44 +0530

    update

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,40 +1,18 @@
 #!/bin/bash
 set +x
 
-# if [[ -f "/var/www/composer.json" ]] ;
-# then
-#     cd /var/www/
-#     if [[ -d "/var/www/vendor" ]] ;
-#     then
-#         echo "Steps to use Composer optimise autoloader"
-#         composer update --prefer-dist --no-interaction --optimize-autoloader --no-dev
-#         echo "Steps to Clear All Development inputs"
-#         php artisan view:clear
-#         php artisan route:clear
-#         php artisan config:clear
-#         php artisan clear-compiled
-#     else
-#         echo "If composer vendor folder is not installed follow the below steps"
-#         composer install --prefer-dist --no-interaction --optimize-autoloader --no-dev
-#     fi
-
-# fi
-# if [[ "$(ls -A "/var/www/")" ]] ;
-#     then
-#         echo "If the Directory is not empty, please delete the hidden files and directory"
-#     else
-#         composer create-project --prefer-dist laravel/laravel:^{LARAVEL_VERSION}.0 .
-# fi
-# echo "Steps to check application environment variable"
-# if [[ ! -f ".env" ]] ;
-# then
-#     echo ".env file not found"
-#     cp .env.example .env
-# else
-#     echo ".env file exit"
-# fi
-
-php artisan key:generate
+if [ ! -e index.php ] && [ ! -e wp-includes/version.php ]; then
+                echo >&2 "WordPress not found in $PWD - copying now..."
+                tar cf - --one-file-system -C /app/wordpress . | tar xf -
+                echo >&2 "Complete! WordPress has been successfully copied to $PWD"
+fi
+if [[ ! -f "wp-config.php" ]] ;
+then
+    echo "wp-config.php file not found"
+    cp wp-config-sample.php wp-config.php
+else
+    echo "wp-config.php file exit"
+fi
 cp /app/default.conf /etc/nginx/conf.d/default.conf
 rm -rf /var/preview
 if [[ {USER_ID} -gt 0 ]] ;
